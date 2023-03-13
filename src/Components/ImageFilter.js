739b407a7a3aa559bff86e141d8fbe39c3ee990c
Wellington Gabriel Monteiro Batista <wellington.gabriel@metropoles.com>
2023-03-13 15:12:58 -0300

    finalizando troca de tema escuro

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -1,3 +1,21 @@
-export default () => {
-  
+
+import React from 'react'
+import {IconeTema} from '../Components/UI'
+import alimentacao from '../assets/images/alimentacao.svg'
+import outros from '../assets/images/outros.svg'
+import saude from '../assets/images/saude.svg'
+import transporte from '../assets/images/transporte.svg'
+import utilidades from '../assets/images/utilidades.svg'
+
+
+export default (type) => {
+  const Images = {
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Utilidade: <IconeTema src={utilidades} alt="Utilidade" />,
+    Saude: <IconeTema src={saude} alt="Saude" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    Default: <IconeTema src={outros} alt="Default" />,
+  };
+
+  return Images[type] || Images.Default;
 }
